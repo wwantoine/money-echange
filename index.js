47c6cdd09a0c8e5bd2fef3a342cc5dff94bc2b46
William <wwantoine@gmail.com>
2020-08-31 17:47:25 +0700

    adding currencies

--- index.js
+++ index.js
@@ -8,13 +8,28 @@ let fromCurrency = prompt("from currency")
 fromCurrency = fromCurrency.toUpperCase()
 let toCurrency = prompt("to currency")
 toCurrency = toCurrency.toUpperCase()
-let ratio = 23000
+let usdToVnd = 23000
+let usdToEur = 0.84
+let usdToKrw = 1186
+let eurToVnd = 27650
+let eurToKrw = 1415
+let vndToKrw = 0.05
 let result = 0
 
-if(fromCurrency == "VND"){
-    result = (amount / ratio).toFixed(2)
-}else if(fromCurrency == "USD"){
-    result = amount * ratio
+if(fromCurrency == "USD"){
+    if(toCurrency == "VND"){
+        result = amount * usdToVnd
+    }else if(toCurrency == "EUR"){
+        result = (amount * usdToEur).toFixed(2)
+    }else if(toCurrency == "KRW"){
+        result = (amount * usdToKrw)
+    }
+}else if(fromCurrency == "VND"){
+    if(toCurrency == "USD"){
+        result = (amount / usdToVnd).toFixed(2)
+    }else if(toCurrency == "EUR"){
+        result = (amount / eurToVnd).toFixed(2)
+    }
 }
 
 function formatCurrency(type, value) {
